73bc0a92286df8497f2ec20c9b8d77719c7478d1
Dick <dick.abma@bigdatarepublic.nl>
2020-01-29 16:06:56 +0100

    Write pyarrow parquet to s3

--- python-requests/api_calls.py
+++ python-requests/api_calls.py
@@ -3,6 +3,9 @@ import asyncio
 import aiohttp
 from typing import List
 import os
+import pyarrow as pa
+
+
 
 @dataclass
 class Data:
@@ -33,10 +36,23 @@ def handler(event, context):
             [os.environ["API"] for _ in range(100)] # todo make var
         )
     )
-    for i in results:
-        print(deserialize(i))
-    return f"{len(results)} responses"
 
+    datas = [deserialize(i) for i in results]
+    dataclasses_to_parquet(datas)
+    return f"{len(datas)} responses"
+
+
+
+def dataclasses_to_parquet(dcs: List[Data]):
+    timestamps = [i.datetime for i in dcs]
+    timestamps_array = pa.array(timestamps, type=pa.string())
+
+    values = [i.values for i in dcs]
+    values_array = pa.array(values, type=pa.list_(pa.int32()))
+    table = pa.Table.from_arrays(arrays=[timestamps_array, values_array], names=["timestamp", "values"])
+    table.to_pandas().to_parquet("s3://bdr-go-blog/dump/python.parquet")
 
 if __name__ == "__main__":
     handler(None, None)
+
+    # print(deserialize({"datetime": "2020-01-27T07:07:27.744489", "values": [2210, 13260, 26837, 30913, 46491, 95062, 41528]}))
--- python-requests/deploy-python-lambda.sh
+++ python-requests/deploy-python-lambda.sh
@@ -1,12 +1,12 @@
 export AWS_DEFAULT_REGION=eu-west-1
 export AWS_PROFILE=bdr
-
+rm -r lambda-packaged/
 docker run --rm -v "$(pwd)":/foo -w /foo lambci/lambda:build-python3.7 \
  pip install -r requirements.txt -t lambda-packaged/
 
 
 cp api_calls.py lambda-packaged/
-cd lambda-packaged && zip -r python-requests.zip *
+cd lambda-packaged && zip -rq python-requests.zip *
 
 aws s3 cp python-requests.zip s3://bdr-go-blog
 
