64ef9126d5ffcee9b087a90c2703a8485d4e045e
Dick <dick.abma@bigdatarepublic.nl>
2020-02-02 18:12:15 +0100

    write go perquet file from data responses

--- go-requests/requests.go
+++ go-requests/requests.go
@@ -5,6 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/xitongsys/parquet-go-source/s3"
+	"github.com/xitongsys/parquet-go/parquet"
+	"github.com/xitongsys/parquet-go/source"
+	"github.com/xitongsys/parquet-go/writer"
+	"log"
 	"net/http"
 	"os"
 	"sync"
@@ -12,36 +17,93 @@ import (
 
 var api = os.Getenv("API")
 
-
 type Data struct {
-	Datetime string `json: datetime` // Name conversion
-	Values   []int  `json: values`
+	Datetime string  `json:"datetime" parquet:"name=datetime, type=UTF8"` // Name conversion
+	Values   []int64 `json:"values" parquet:"name=values, type=LIST, valuetype=INT64"`
 }
 
-func Deserialize(r http.Response) Data {
+func DataParquetWriter(datas chan Data) (*writer.ParquetWriter, source.ParquetFile, error) {
+	log.Println("generating parquet file")
+	ctx := context.Background()
+	bucket := "bdr-go-blog"
+	key := "foobar.parquet"
+	fileWriter, err := s3.NewS3FileWriter(ctx, bucket, key, nil)
+	parquetWriter, err := writer.NewParquetWriter(fileWriter, new(Data), int64(cap(datas)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	parquetWriter.CompressionType = parquet.CompressionCodec_SNAPPY
+
+	return parquetWriter, fileWriter, nil
+}
+
+func AddDataToParquet(wg *sync.WaitGroup, pw *writer.ParquetWriter, datas chan Data) {
+	data := <-datas
+	fmt.Println(data)
+	if err := pw.Write(data); err != nil {
+		log.Fatal(err)
+	}
+	wg.Done()
+}
+
+func CloseParquetWrite(pw *writer.ParquetWriter, fw source.ParquetFile) error {
+	if err := pw.WriteStop(); err != nil {
+		return err
+	}
+
+	err := fw.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func Deserialize(r http.Response, datas chan<- Data) Data {
 	data := Data{}
 	_ = json.NewDecoder(r.Body).Decode(&data) // Deserialize body
+	datas <- data
 	_ = r.Body.Close()
 	return data
 }
 
-func Get(wg *sync.WaitGroup) {
-	r, _ := http.Get(api) 			// Make get request
-	fmt.Println(Deserialize(*r))    // Print body
-	wg.Done() 						// Communicate end of goroutine
+func Get(wg *sync.WaitGroup, datas chan<- Data) {
+	defer wg.Done()       // Communicate end of goroutine
+	r, _ := http.Get(api) // Make get request
+	fmt.Println(r.Status)
+	Deserialize(*r, datas) // Print body
+
 }
 
 func Handler(_ context.Context, _ interface{}) (string, error) {
-	wg := sync.WaitGroup{}
-	for i := 0; i < 100; i++ {		// make 1000 requests
-	wg.Add(1) 				// increment counter
-		go Get(&wg)					// <<<=== Run Get concurrently
+	numberOfRequests := 100
+	wgGet := sync.WaitGroup{}
+
+	datas := make(chan Data, 100)
+
+	for i := 0; i < numberOfRequests; i++ { // make 1000 requests
+		wgGet.Add(1)
+		go Get(&wgGet, datas) // <<<=== Run Get concurrently
 	}
-	wg.Wait() 						// Wait untill all responses obtained
+	wgGet.Wait() // Wait until all responses obtained
+
+	wgParquet := sync.WaitGroup{}
+	parquetWriter, fileWriter, err := DataParquetWriter(datas)
+	for i := 0; i < numberOfRequests; i++ {
+		wgParquet.Add(1)
+		go AddDataToParquet(&wgParquet, parquetWriter, datas)
+	}
+	wgParquet.Wait()
+	err = CloseParquetWrite(parquetWriter, fileWriter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return "Processed requests", nil
 }
 
 func main() {
-	//Handler(nil, nil)
+	//_, _ = Handler(nil, nil)
 	lambda.Start(Handler)
 }
