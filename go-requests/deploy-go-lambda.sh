c37fb61cfef70cf791b3c00bd9f344eb4cc725d8
Dick <dick.abma@bigdatarepublic.nl>
2020-02-12 18:00:18 +0100

    include deployment to go workflow

--- go-requests/deploy-go-lambda.sh
+++ go-requests/deploy-go-lambda.sh
@@ -1,8 +1,8 @@
 export AWS_DEFAULT_REGION=eu-west-1
-export AWS_PROFILE=bdr
+export AWS_PROFILE=github
 
 GOOS=linux GOARCH=amd64 go build requests.go
 zip -q go-requests.zip requests
 aws s3 cp go-requests.zip s3://bdr-go-blog
 
-aws lambda update-function-code --function-name api-calls-go --s3-bucket bdr-go-blog --s3-key go-requests.zip
+aws lambda update-function-code --function-name api-calls-go --s3-bucket bdr-go-blog --s3-key go-requests.zip > /tmp/dumpgo
