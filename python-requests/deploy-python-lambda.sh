d497559718e55b70e1029f1fb80d89eab8e0006d
Dick <dick.abma@bigdatarepublic.nl>
2020-02-12 18:29:46 +0100

    add deployment of lambda to shell script.

--- python-requests/deploy-python-lambda.sh
+++ python-requests/deploy-python-lambda.sh
@@ -1,9 +1,21 @@
 export AWS_DEFAULT_REGION=eu-west-1
 export AWS_PROFILE=github
+rm -r python
 rm -r lambda-packaged/
-docker run --rm -v "$(pwd)":/foo -w /foo lambci/lambda:build-python3.7 \
- pip install -r requirements.txt -t lambda-packaged/
 
+set -e  # bash don't continue on error
+
+docker run --rm -v "$(pwd)":/foo -w /foo lambci/lambda:build-python3.7 \
+ pip install -r requirements.txt -t python/lib/python3.7/site-packages/
+
+zip -rq9 python-requests-layer.zip python
+
+aws s3 cp python-requests-layer.zip s3://bdr-go-blog/python-requests-layer.zip
+
+aws lambda publish-layer-version \
+  --layer-name frequent-layer \
+  --content S3Bucket=s3://bdr-go-blog,S3Key=python-requests-layer.zip \
+  --compatible-runtimes python3.7
 
 cp *.py lambda-packaged/
 cd lambda-packaged && zip -rq python-requests.zip *
