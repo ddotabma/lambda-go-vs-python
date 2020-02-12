e0aaddbef91ec7f629158231f3e7ad3c09ab93b1
Dick <dick.abma@bigdatarepublic.nl>
2020-02-12 18:29:32 +0100

    add lambda layer to python lambda

--- terraform/lambda-python.tf
+++ terraform/lambda-python.tf
@@ -7,7 +7,8 @@ resource "aws_lambda_function" "python_requests" {
   role = aws_iam_role.requests.arn
   timeout = 30
   memory_size = 320
-
+  //  layers = [
+  //    var.python_requests_layer_arn]
   environment {
     variables = {
       API = var.API
@@ -16,3 +17,10 @@ resource "aws_lambda_function" "python_requests" {
 }
 
 
+resource "aws_lambda_layer_version" "python_requests_layer" {
+  layer_name = "python-requests-layer"
+  s3_bucket = aws_s3_bucket.blog_bucket.bucket
+  s3_key = "python-requests-layer.zip"
+  compatible_runtimes = [
+    "python3.7"]
+}
